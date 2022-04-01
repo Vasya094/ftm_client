1df73069f8b07165be22d303d24a7c3cebd700e7
rinatkhabibulaev <fLukYsat2DXaf9HuRFQK>
2022-04-01 07:02:29 +0300

    4

--- src/actions/applications.ts
+++ src/actions/applications.ts
@@ -3,8 +3,7 @@ import { MainFiltersTypes, NewApplicationTypes } from "../types"
 
 console.log(process.env);
 
-const apiUrl =
-  process.env.NODE_ENV === "production"
+const apiUrl = process.env.NODE_ENV === "production"
     ? process.env.REACT_APP_API_PROD
     : process.env.REACT_APP_API
 
